3660fadd3d57d2c81ecde5765a8ad17a37410642
yuandominic <yuandominic@users.noreply.github.com>
2020-12-31 20:15:54 +0800

    Initial commit

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+   private Transform _transform;
+
+   private void Awake()
+   {
+      _transform = GetComponent<Transform>();
+   }
+
+   private void LateUpdate()
+   {
+      _transform.position = new Vector3(_transform.position.x + Time.deltaTime*3, _transform.position.y, _transform.position.z);
+   }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    private float x, y, z;
+    public float speed = 1;
+    public float amplitude;
+    public bool Sin = false;
+
+    public GameObject sinUI,cosUI;
+    private Text cosText, sinText;
+    private Rigidbody _rigidbody;
+    private Transform _transform;
+
+    private void Awake()
+    {
+        sinText = sinUI.GetComponent<Text>();
+        cosText = cosUI.GetComponent<Text>();
+        _rigidbody = GetComponent<Rigidbody>();
+        _transform = GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        if (Sin)
+        {
+            y = Mathf.Sin(Time.time*speed)*amplitude ;
+            _transform.position = new Vector3(_transform.position.x+Time.deltaTime*3,y,z);
+            sinText.text = $"Sin = {Mathf.Sin(Time.time*speed)*3:#0.00}";
+        }
+        else
+        {
+            y = Mathf.Cos(Time.time*speed)*amplitude ;
+            _transform.position = new Vector3(_transform.position.x+Time.deltaTime*3,y,z);
+            cosText.text = $"Cos = {Mathf.Cos(Time.time*speed)*3:#0.00}";
+
+        }
+
+    }
+}
